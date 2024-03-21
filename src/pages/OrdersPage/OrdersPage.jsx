a54a652f5d8381c3a0e37203dd113b55e49077ca
Venera <veneraomurzakkyzy@gmail.com>
2024-03-21 17:06:46 +0600

    T4-252 add statuses of orders

--- src/pages/OrdersPage/OrdersPage.jsx
+++ src/pages/OrdersPage/OrdersPage.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import MainLayout from "../../layout/MainLayout/MainLayout";
 import OrderCard from "../../components/OrderCard/OrderCard";
+import OrderStatusItems from "../../components/OrderStatusItems/OrderStatusItems";
 import styles from "./OrdersPage.module.css";
 import classnames from "classnames";
 
 const OrdersPage = () => {
+  const [selectedOrderType, setSelectedOrderType] = useState("takeaway");
+  const handleOrderTypeSelection = (ordertype) => {
+    setSelectedOrderType(ordertype);
+  };
+
   return (
     <MainLayout>
+      <OrderStatusItems selectedOrderType={selectedOrderType} />
       <div className={classnames(styles.order_container)}>
         <OrderCard />
         <OrderCard />
